ece7047a6fd24ebeb9b7d217aa12b41519365691
Maria Fernanda Do Monte Nogueira <maferdomonte@gmail.com>
2025-04-16 13:45:12 -0300

    Add files via upload

---
+++ SettingsScreen.js
@@ -0,0 +1,22 @@
+//Screens/SettingsScreen.js
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+function SettingsScreen(){
+    return(
+        <View style ={Styles.container}>
+            <Text>Settings Screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
+
+export default SettingsScreen;
+
